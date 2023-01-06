b8a1c8c5854375b56c15e65ee1b271c1b8c95c5c
FlorinChertes <FlorinChertes@computer.org>
2023-01-06 19:03:04 +0100

    print_size

---
+++ main_01.c
@@ -0,0 +1,19 @@
+#include "print_size.h"
+#include "convert.h"
+
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, world!\n");
+
+	float celsius = 20;
+	float fahrenheit = convert_celsius_to_fahrenheit(celsius);
+	printf("celsius: %f, fahrenheit: %f\n", celsius, fahrenheit);
+
+	printSizes();
+
+
+	return 0;
+}
